fa8fd60cedb013d3699343906db2f47acd12f9a8
Jan-Jaap Driessen <devnull@localhost>
2011-08-03 11:10:07 +0200

    Make tiny_mce_popup.js available as a Fanstatic Resource.

--- js/tinymce/__init__.py
+++ js/tinymce/__init__.py
@@ -3,3 +3,5 @@ from fanstatic import Library, Resource
 library = Library('tinymce', 'resources')
 
 tinymce = Resource(library, 'tiny_mce_src.js', minified='tiny_mce.js')
+
+tinymcepopup = Resource(library, 'tiny_mce_popup.js')
