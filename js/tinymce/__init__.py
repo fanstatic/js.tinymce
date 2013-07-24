17872af0b9561827bbc73b727212d67617d259fb
Vanč Levstik <vanc.levstik@gmail.com>
2013-07-25 01:56:28 +0200

    Added tinymce 4.0.1, and changed init to support new structure

--- js/tinymce/__init__.py
+++ js/tinymce/__init__.py
@@ -2,6 +2,4 @@ from fanstatic import Library, Resource
 
 library = Library('tinymce', 'resources')
 
-tinymce = Resource(library, 'tiny_mce_src.js', minified='tiny_mce.js')
-
-tinymcepopup = Resource(library, 'tiny_mce_popup.js')
+tinymce = Resource(library, 'tinymce.js', minified='tinymce.min.js')
