414a82613aa5c2453b35b71f920ab2da0b2ce4cd
Michael Howitz <mh@gocept.com>
2017-03-07 09:42:27 +0100

    Preparing release 4.5.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 # For example, a packaging bugfix release version 1.4.4 of the
 # js.jquery package would be version 1.4.4-1 .
 
-version = '4.5.4.dev0'
+version = '4.5.4'
 
 
 def read(*rnames):
